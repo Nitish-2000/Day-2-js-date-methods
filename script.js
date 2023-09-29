a87f513f7759623ef65178284bcd509c3adc3f9e
Nitish-2000 <nitishmani111@gmail.com>
2023-09-29 23:08:26 +0530

    Updated code 0

---
+++ script.js
@@ -0,0 +1,40 @@
+//date get methods
+"use scrict"
+var a = new Date();
+console.log(a);
+var b = a.getDay();
+console.log(b);
+var c = a.getDate();
+console.log(c);
+var b=a.getFullYear();
+console.log(b);
+var b = a.getMonth();
+console.log(b);
+var b = a.getTime();
+console.log(b);
+var b = a.getHours();
+console.log(b);
+var b = a.getMinutes();
+console.log(b);
+var b = a.getSeconds();
+console.log(b);
+var b = a.getMilliseconds();
+console.log(b);
+
+// date set methods
+var a = new Date();
+console.log(a);
+a.setDate(11);
+console.log(a);
+a.setMonth(0);
+console.log(a);
+a.setFullYear(2000);
+console.log(a);
+a.setHours(12);
+console.log(a);
+a.setMinutes(0);
+console.log(a);
+a.setSeconds(0);
+console.log(a);
+a.setTime(6846545454);
+console.log(a);
